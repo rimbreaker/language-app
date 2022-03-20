ece74ac35a9020eac5e385aae7462a6fd4f67071
Janek <janekkacperczyk@gmail.com>
2022-03-20 20:29:15 +0100

    added init for simple flow, basis for firestore interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,87 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import { getFirestore, collection, setDoc, doc, deleteDoc, onSnapshot, query, where, orderBy, limit, getDoc, getDocFromCache, } from 'firebase/firestore'
+import { getFirestore, collection, setDoc, doc, deleteDoc, onSnapshot, query, where, orderBy, limit, getDoc, getDocFromCache, DocumentData, } from 'firebase/firestore'
 import { initializePerformance } from 'firebase/performance'
 import { initializeAnalytics } from 'firebase/analytics'
 import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import availableLangs from './encoding.json'
+import CoursePage from './CoursePage';
+import { Switch, Route, withRouter, Redirect, RouteComponentProps } from 'react-router-dom'
+import { useHistory } from 'react-router';
 
 const firebaseConfig = {
+
 };
 
 const app = initializeApp(firebaseConfig)
 
 const auth = getAuth()
 const db = getFirestore()
-const langColRef = collection(db, 'languages')
 const coursesRef = collection(db, 'activeCourses')
 initializePerformance(app)
 initializeAnalytics(app)
 
 function App() {
 
-  const authUnsub = onAuthStateChanged(auth, (user) => console.log(user))
-  const dbUnsub = onSnapshot(langColRef, () => console.log("dupa"))
+  //const authUnsub = onAuthStateChanged(auth, (user) => console.log(user))
   const [user] = useAuthState(auth)
 
-  const q = query(coursesRef, limit(10))
+  const q = query(coursesRef, where("userData", "==", doc(db, 'usersData/', user?.email || 'lol')), limit(10))
 
-  const [courses] = useCollectionData(q);
+  const userCoursesUnsub = user ? onSnapshot(q, () => console.log("change in courses")) : () => null
 
-  const deleteRussian = () => {
-    const docRef = doc(db, 'languages', 'RU')
-
-    deleteDoc(docRef)
+  const logout = () => {
+    signOut(auth);
+    userCoursesUnsub();
   }
-
-  const logout = () => { signOut(auth); authUnsub(); dbUnsub(); }
 
   const login = () => {
     signInWithPopup(auth, new GoogleAuthProvider()).then((user) => {
-      const { uid, photoURL, displayName, email } = user.user
-      setDoc(doc(db, 'usersData', email || "lol"), { uid, photoURL, displayName, email })
-      console.log(user)
+      if ((user.user.metadata as any).lastLoginAt - (user.user.metadata as any).createdAt < 10) {
+        console.log("ADDING NEW USER")
+        const { uid, photoURL, displayName, email } = user.user
+        setDoc(doc(db, 'usersData', email || "lol"), { uid, photoURL, displayName, email })
+      }
     })
   }
 
-  const addCourse = () => {
-    const courseName = (user?.email || 'lol') + 'US'
-    setDoc(doc(db, 'activeCourses', courseName), { userData: doc(db, 'usersData/', user?.email || 'lol'), percentageCompleted: 0, learnedWords: null })
-  }
 
   return (
     <div className="App">
+      {user ? <button onClick={() => logout()}>logout</button> : <button onClick={() => login()}>login</button>}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={() => deleteRussian()}>delete russian</button>
-        <button onClick={() => login()}>login</button>
-        <button onClick={() => logout()}>logout</button>
-        {user && <button onClick={() => addCourse()}>add US course</button>}
-        {
-          courses?.map((course) => {
-            return (<>course <button onClick={async () => {
-              console.log(course, course.userData);
-              console.log((await getDocFromCache(course.userData).catch(() => getDoc(course.userData))).data())
+        <Switch>
+          <Route exact path='/course/:courseLang' component={CoursePageByCourseLang} />
+          <Route exact path='/' component={HomePage} />
+          <Redirect to='/' />
+        </Switch>
 
-            }}>log data</button></>)
-          })}
       </header>
     </div>
   );
 }
 
-export default App;
+const CoursePageByCourseLang = ({ match }: RouteComponentProps) => {
+  return <CoursePage auth={auth} db={db} courseLang={(match.params as {} & { courseLang: string }).courseLang} />
+}
+
+const HomePage = () => {
+  const [user] = useAuthState(auth)
+
+  const q = query(coursesRef, where("userData", "==", doc(db, 'usersData/', user?.email || 'lol')), limit(10))
+
+  const [courses] = useCollectionData(q);
+
+  const [newCourseLanguage, setNewCourseLanguage] = useState('english')
+
+  const deleteCourse = (course: DocumentData) => {
+    deleteDoc(doc(db, 'activeCourses', course.id))
+  }
+
+
+  const createNewCourse = () => {
+    const languageEncoding = availableLangs[newCourseLanguage as keyof typeof availableLangs]
+    if (!courses?.find(course => course.language === languageEncoding)) {
+      const courseName = (user?.email || 'failedUser') + languageEncoding
+      setDoc(doc(db, 'activeCourses', courseName), { id: courseName, userData: doc(db, 'usersData/', user?.email || ''), percentageCompleted: 0, learnedWords: null, language: languageEncoding })
+    } else {
+      alert('you already ahve a course in that language')
+    }
+  }
+  const history = useHistory()
+  return (<>
+    {user && <>
+      <p> select language:  <select value={newCourseLanguage} onChange={(e) => setNewCourseLanguage(e.target.value)}>
+        {Object.entries(availableLangs).map(([key]) => (
+          <option key={key} >{key}</option>
+        ))}
+      </select>
+        <button onClick={() => createNewCourse()}>add course</button>
+      </p>
+      <h3>your courses:</h3>
+      {
+        courses?.map((course, i) => {
+          return (<div key={i}>
+            {course.language} course: {course.percentageCompleted}% completed
+            <button onClick={async () => {
+              history.push('/course/' + course.language)
+              console.log(course, course.userData);
+              console.log((await getDocFromCache(course.userData).catch(() => getDoc(course.userData))).data())
+            }}>
+              open course
+            </button>
+            <button onClick={() => deleteCourse(course)}>
+              delete course
+            </button>
+          </div>)
+        })}
+    </>}</>
+  )
+}
+
+export default withRouter(App);
