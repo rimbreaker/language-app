3071831ece9e07539d38ed34724c58b4664bf297
Janek <janekkacperczyk@gmail.com>
2022-04-10 15:35:40 +0200

    redirect impovements

--- client/index.js
+++ client/index.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/auth',(req,res)=>{
-  const code=req.query.code
-  res.redirect(200,'https://lyrson-client.herokuapp.com/#/auth?code='+code)
-})
 
 app.use(express.static(__dirname + '/build'));
 
+app.get('/auth',(req,res)=>{
+  const code=req.query.code
+  res.redirect(200,'/#/auth?code='+code)
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
