2e56cee278cc64e779f10953f26774ad76a08c44
Janek <janekkacperczyk@gmail.com>
2022-04-13 13:38:26 +0200

    translator fix

--- server/src/controllers/translationController.ts
+++ server/src/controllers/translationController.ts
@@ -1,11 +1,14 @@
 const { translate } = require("bing-translate-api");
 import { Request, Response } from "express";
+import loactionToLangsMap from "../loactionToLAngs";
 
 const translator = async (req: Request, res: Response) => {
   const phrase = decodeURI(req.params.phrase);
-  const fromLang = req.query.from === "DK" ? "da" : req.query.from;
-  const toLang = req.query.to ?? "en";
-  const lang = fromLang ? fromLang.toString().toLowerCase() : undefined;
+  const fromLang =
+    loactionToLangsMap[(req.query.from as string).toLocaleUpperCase()][0];
+  const toLang =
+    loactionToLangsMap[(req.query.to as string).toUpperCase()][0] ?? "en";
+  const lang = fromLang ?? undefined;
   const translation = await translate(phrase, lang, toLang, true);
 
   res.json(translation);
