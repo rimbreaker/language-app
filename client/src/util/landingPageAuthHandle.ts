a9543d6db4d12ec83ed388f2be1f47f2b277c423
Janek <janekkacperczyk@gmail.com>
2022-06-06 13:27:30 +0200

    ffs2

--- client/src/util/landingPageAuthHandle.ts
+++ client/src/util/landingPageAuthHandle.ts
@@ -1,8 +1,7 @@
 export const landingPageAuthHandle = (history: any) => {
   const href = window.location.href;
-  const code = href.slice(href.indexOf("code=") + 5, href.indexOf("#/"));
-
-  if (!code.includes("/")) {
+  if (href.includes("com/auth?code") && href.slice(-2) === "#/") {
+    const code = href.slice(href.indexOf("code=") + 5, href.indexOf("#/"));
     history.push(`/auth?code=${code}`);
   }
 };
